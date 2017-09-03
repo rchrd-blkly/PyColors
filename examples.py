7cf9b3cc36725a764fe796a6530c461d090bd991
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2017-09-03 13:17:26 +0200

    Add files via upload

---
+++ examples.py
@@ -0,0 +1,31 @@
+from pycolors import *
+"""
+This script just shows how each modification looks like when using the PyColors module
+"""
+print("\nThis is how each modification looks like:")
+print("-"*45)
+
+#text
+print(text.bold+"Bold"+end)
+print(text.underline+"Underline"+end)
+print(text.blink+"Blink"+end)
+print(text.invert+"Invert"+end)
+print(text.purple+"Purple"+end)
+print(text.cyan+"Cyan"+end)
+print(text.darkcyan+"Darkcyan"+end)
+print(text.blue+"Blue"+end)
+print(text.yellow+"Yellow"+end)
+print(text.red+"Red"+end)
+print(text.magenta+"Magenta"+end)
+print(text.white+"White"+end)
+print(text.black+"Black")
+
+#highlight
+print(highlight.red+"Red"+end)
+print(highlight.black+"Black"+end)
+print(highlight.green+"Green"+end)
+print(highlight.yellow+"Yellow"+end)
+print(highlight.blue+"Blue"+end)
+print(highlight.magenta+"Magenta"+end)
+print(highlight.cyan+"Cyan"+end)
+print(highlight.white+"White"+end)
---
+++ pycolors.py
@@ -0,0 +1,29 @@
+#Siddharth Dushantha
+#3 September 2017
+class text:
+   purple = "\033[95m"
+   cyan = "\033[96m"
+   darkcyan = "\033[36m"
+   blue = "\033[94m"
+   green = "\033[92m"
+   yellow = "\033[93m"
+   red = "\033[91m"
+   magenta = "\033[35m"
+   white = "\033[37m"
+   bold = "\033[1m"
+   underline = "\033[4m"
+   blink = "\033[5m"
+   invert = "\033[7m"
+   black = "\033[30m"
+
+class highlight:
+   red = "\033[41m"
+   black = "\033[40m"
+   green = "\033[42m"
+   yellow = "\033[43m"
+   blue = "\033[44m"
+   magenta = "\033[45m"
+   cyan = "\033[46m"
+   white = "\033[47m"
+
+end = "\033[0m" #this stops the attributes or else your whole shell will have that attribute
